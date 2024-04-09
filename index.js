71245f0fe007c5fa591002234e4a7df1efa2c26d
AnthonyB0511 <137073922+AnthonyB0511@users.noreply.github.com>
2024-04-09 13:32:49 +0200

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-
+const nodemailer = require("nodemailer");
 const app = express();
 
 const port = process.env.PORT;
@@ -24,6 +24,41 @@ app.use(routes);
 app.get("/", (_, res) => {
     res.send(JSON.stringify("API WORKING"));
 });
+app.post('/send', (req, res) => {
+    const { name, firstname, email, subject, message } = req.body;
+    console.log(name);
+    // Configurer le transporteur Nodemailer
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: process.env.MAIL,
+                pass: process.env.PASSWORDNODEMAILER,
+            },
+        });
+
+        // Options du message
+        const mailOptions = {
+            from: email,
+            to: process.env.MAIL, // Adresse e-mail de destination
+            subject: subject,
+            text: `Nom: ${name}\nPrénom: ${firstname}\nEmail: ${email}\nMessage: ${message}`,
+        };
+
+        // Envoyer l'email
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error(error);
+                res.send('Erreur lors de l\'envoi de l\'email.');
+            } else {
+                res.status(200).json('Ok');
+            }
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+);
 app.listen(port, "0.0.0.0", () => {
     console.log(`serveur Node écoutant sur le port ${port}`);
 });
